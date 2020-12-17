c460c451026ce531e6b783cb06c8e5813ce4fd9d
mottox2 <y.mottox2@gmail.com>
2020-12-17 19:06:47 +0900

    Init project

---
+++ 01-react/src/App.js
@@ -0,0 +1,13 @@
+import styles from "./App.module.css";
+
+const App = () => {
+  return (
+    <div className={styles.container}>
+      <p className={styles.message}>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+    </div>
+  );
+};
+
+export default App;
