c60eba0f8b832638eba5dba07eb62b3e8f50877b
mottox2 <y.mottox2@gmail.com>
2020-12-17 19:34:34 +0900

    Add styles for heading and profile

--- 01-react/src/App.js
+++ 01-react/src/App.js
@@ -2,7 +2,19 @@ import styles from "./App.module.css";
 import Layout from "./Layout";
 
 const App = () => {
-  return <Layout>Hello Gatsby</Layout>;
+  return (
+    <Layout>
+      <h2 className={styles.heading}>About</h2>
+      <p className={styles.profile}>
+        都内でウェブサービスやスマホアプリを作るフロントエンドエンジニア・UIデザイナーです。
+        新規事業の爆速立ち上げや、使いやすいSPAの開発が得意です。
+      </p>
+      <h2 className={styles.heading}>Posts</h2>
+      <p>Hello Gatsby</p>
+      <h2 className={styles.heading}>Works</h2>
+      <p>Hello Gatsby</p>
+    </Layout>
+  );
 };
 
 export default App;
