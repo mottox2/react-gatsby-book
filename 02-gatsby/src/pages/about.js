7ec55d201a5a00dff9ef838a473e4bb5230ca643
mottox2 <y.mottox2@gmail.com>
2020-12-19 13:08:53 +0900

    Add react-helmet

---
+++ 02-gatsby/src/pages/about.js
@@ -0,0 +1,15 @@
+import * as React from "react";
+
+const AboutPage = (props) => {
+  return (
+    <div>
+      <Helmet>
+        <title>My Gatsby Site</title>
+        <meta name="description" content="My first gatsby website" />
+      </Helmet>
+      <p>Hello Gatsby</p>
+    </div>
+  );
+};
+
+export default AboutPage;
