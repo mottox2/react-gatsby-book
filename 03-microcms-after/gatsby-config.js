d751b3953ba5178833f0678de0225c84078c0b4c
mottox2 <y.mottox2@gmail.com>
2020-12-23 04:19:08 +0900

    Use gatsby-source-microcms

--- 03-microcms-after/gatsby-config.js
+++ 03-microcms-after/gatsby-config.js
@@ -2,5 +2,22 @@ module.exports = {
   siteMetadata: {
     title: "03-microcms",
   },
-  plugins: ["gatsby-plugin-react-helmet"],
+  plugins: [
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: "gatsby-source-microcms",
+      options: {
+        apiKey: process.env.MICROCMS_API_KEY,
+        serviceId: "gatsby-microcms-blog",
+        apis: [
+          {
+            endpoint: "post",
+          },
+          {
+            endpoint: "category",
+          },
+        ],
+      },
+    },
+  ],
 };
