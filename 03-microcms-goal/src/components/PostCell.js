648faecca7b5c831d09f8aeb3481f2b736bb8a4a
mottox2 <y.mottox2@gmail.com>
2021-02-14 10:39:11 +0900

    Use file system route api

--- 03-microcms-goal/src/components/PostCell.js
+++ 03-microcms-goal/src/components/PostCell.js
@@ -4,14 +4,14 @@ import styles from "./PostCell.module.css";
 
 const PostCell = (props) => {
   const { title, content, category, publishedAt, slug, thumbnail } = props.post;
-  const plainContent = content.replace(/(<([^>]+)>)/gi, "");
+  const plainContent = (content || "").replace(/(<([^>]+)>)/gi, "");
   return (
     <Link to={`/posts/${slug}`} className={styles.container}>
       <div className={styles.thumbnailWrapper}>
         <img src={thumbnail?.url} alt={title} />
       </div>
       <div className={styles.content}>
-        <p className={styles.category}>{category.name}</p>
+        {/* <p className={styles.category}>{category.name}</p> */}
         <div className={styles.title}>{title}</div>
         <p className={styles.description}>{plainContent}</p>
         <time className={styles.publishedAt}>{publishedAt}</time>
